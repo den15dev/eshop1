b702ec1f100b1b15d4390031692d3af6b4e37849
den15 <dendangler@gmail.com>
2023-08-05 19:15:11 +0300

    v0.9.9.4 Rich preview added.

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -4,6 +4,16 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>@yield('page_title', config('app.name'))</title>
+    <meta name="description" content="Электроника для дома и работы: компьютерные комплектующие, бытовая техника и многое другое.">
+    <meta name="twitter:card" content="summary">
+    <meta name="twitter:title" content="Магазин электроники">
+    <meta name="twitter:description" content="Электроника для дома и работы: компьютерные комплектующие, бытовая техника и многое другое.">
+    <meta name="twitter:image" content="{{ asset('img/electro_richpreview.png') }}">
+    <meta property="og:title" content="Магазин электроники">
+    <meta property="og:type" content="website">
+    <meta property="og:description" content="Электроника для дома и работы: компьютерные комплектующие, бытовая техника и многое другое.">
+    <meta property="og:image" content="{{ asset('img/electro_richpreview.png') }}">
+    <meta property="og:url" content="{{ route('home') }}">
     <link rel="icon" type="image/png" href="{{ asset('img/electro_logo_16.png') }}" sizes="16x16">
     <link rel="icon" type="image/png" href="{{ asset('img/electro_logo_32.png') }}" sizes="32x32">
     <link rel="icon" type="image/png" href="{{ asset('img/electro_logo_64.png') }}" sizes="64x64">
